f45693014acec0b9feb554492ec243bafc3c806d
kunalkthalautiya <kunalkumarthalautiya@gmail.com>
2025-03-03 16:36:34 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
+
+# Home Page Route
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+# API Route
+@app.route('/api')
+def api():
+    return jsonify({'message': 'Hello from Flask API!'})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
