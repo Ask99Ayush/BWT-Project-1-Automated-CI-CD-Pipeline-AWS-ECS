96074754900f0ac2c2d3b669451500f2d8310118
Ayush Rao Chaudhary <116994510+Ask99Ayush@users.noreply.github.com>
2026-01-08 01:11:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from flask import Flask, jsonify, render_template
 
 app = Flask(__name__)
 
-# Home Page Route
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# API Route
-@app.route('/api')
-def api():
-    return jsonify({'message': 'Hello from Flask API!'})
+@app.route('/health')
+def health():
+    return "OK", 200
+
+@app.route('/version')
+def version():
+    return "DEPLOYMENT VERSION 9", 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
